ba8f479e3b01e228011017ccd8350cc2b43c4908
Patrick Farley <patrickfarley1994@yahoo.com>
2018-05-09 23:21:32 -0400

    db working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('cheese burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('veggie burger', true);
+INSERT INTO burgers (burger_name) VALUES ('plain burger');
